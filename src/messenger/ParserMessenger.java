78302706c63c3a61c168cfd3c6bd89233172dd59
xapharius <mihai.suteu@xapharius.com>
2013-12-11 02:21:57 +0100

    added --input-file info to help

--- src/messenger/ParserMessenger.java
+++ src/messenger/ParserMessenger.java
@@ -16,7 +16,7 @@ public class ParserMessenger {
 		System.out.println("Usage: wget [OPTION]... [URL]...");
 		System.out.println();
 		System.out.println("-O,  --output-document=FILE \t\t write documents to FILE.");
-		System.out.println("     --input-file \t\t\t download URLs found in local file.");
+		System.out.println("     --input-file \t\t\t download URLs found in local file. Each URL on a different line.");
 		System.out.println("-t,  --tries=NUMBER    \t\t\t set number of retries to NUMBER.");
 		System.out.println("     --spider \t\t\t\t to see if remote file is available.");
 		System.out.println();
